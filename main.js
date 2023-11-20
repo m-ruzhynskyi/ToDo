8b7131ae5f1c5cbf51d1e803737b4ba155a7e1eb
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2023-11-20 12:56:47 +0200

    add additional settings block, css

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ class NewTask {
     #button1;
     #button2;
     #mainDiv;
+    #buttonDiv;
+    #userX;
+    #element;
+    #userY;
+    #settBlock;
     constructor(task) {
         this.#task = task;
     }
     get add(){
         this.#mainDiv = document.createElement('div');
+        this.#buttonDiv =  document.createElement('div');
         this.#mainDiv.style.maxWidth = '1120px';
         this.#mainDiv.classList.add('task')
 
@@ -26,18 +32,34 @@ class NewTask {
         this.#p.textContent = this.#task;
 
         this.#button1 = document.createElement('button');
-        this.#button1.style.marginLeft = '1000px'
+        this.#button1.innerHTML = '<img src="./assets/img/threeDots.png" alt="threeDots" class="mainImgs">';
+        this.#button1.classList.add('buttonsEditMain');
+        this.#button1.addEventListener('click', (e) => {this.additionalSettings(e)})
         this.#button2 = document.createElement('button');
+        this.#button2.innerHTML = '<img src="./assets/img/bin.png" alt="bin" class="mainImgs" >';
+        this.#button2.classList.add('buttonsEditMain');
 
         this.#div.append(this.#input);
         this.#div.append(this.#p);
+        this.#buttonDiv.append(this.#button1);
+        this.#buttonDiv.append(this.#button2);
         this.#mainDiv.append(this.#div);
-        this.#mainDiv.append(this.#button1);
-        this.#mainDiv.append(this.#button2);
+        this.#mainDiv.append(this.#buttonDiv);
 
         return this.#mainDiv;
     }
-
+    additionalSettings(e){
+        this.#userY = e.pageY;
+        this.#element = document.querySelector('.addSett')
+        if (this.#element.style.display === 'none'){
+            this.#element.classList.add('addSett');
+            this.#element.style.top = `${this.#userY}px`;
+            this.#element.style.display = 'flex';
+            this.#element.style.flexDirection = 'column';
+        }else{
+            this.#element.style.display = 'none';
+        }
+    }
 }
 
 let night = document.querySelector('.moon');
