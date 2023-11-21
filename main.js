a58d90db88923db148f82db9e6d2e2b3e6b35051
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2023-11-21 22:34:53 +0200

    finish

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 class NewTask {
+    static idCounter = 0;
+
     #task;
     #input;
     #div;
@@ -9,93 +11,126 @@ class NewTask {
     #buttonDiv;
     #element;
     #userY;
+    #nav;
+    #button3;
+    #taskId;
+
     constructor(task) {
+        NewTask.idCounter += 1;
+        this.#taskId = NewTask.idCounter;
         this.#task = task;
     }
-    get add(){
+
+    get add() {
         this.#mainDiv = document.createElement('div');
-        this.#buttonDiv =  document.createElement('div');
+        this.#buttonDiv = document.createElement('div');
+
         this.#mainDiv.style.maxWidth = '1120px';
         this.#mainDiv.classList.add('task')
+
+        this.#nav = document.createElement('nav');
+        this.#nav.classList.add('addSett')
 
         this.#div = document.createElement('div');
         this.#div.style.display = 'flex';
         this.#div.style.paddingLeft = '10px';
+        this.#div.id = this.#taskId
 
         this.#input = document.createElement('input');
         this.#input.type = 'checkbox';
         this.#input.id = 'check';
         this.#input.style.cursor = 'pointer ';
-        this.#input.addEventListener('click', (e) => {this.taskCompleted(e)})
+        this.#input.addEventListener('click', (e) => {
+            this.taskCompleted(e)
+        })
 
         this.#p = document.createElement('p');
         this.#p.textContent = this.#task;
+        this.#p.id = this.#taskId
 
         this.#button1 = document.createElement('button');
         this.#button1.innerHTML = '<img src="./assets/img/threeDots.png" alt="threeDots" class="mainImgs">';
         this.#button1.classList.add('buttonsEditMain');
-        this.#button1.addEventListener('click', (e) => {this.additionalSettings(e)})
+        this.#button1.addEventListener('click', (e) => {
+            this.edit(this.#p)
+        })
+
         this.#button2 = document.createElement('button');
         this.#button2.innerHTML = '<img src="./assets/img/bin.png" alt="bin" class="mainImgs" >';
         this.#button2.classList.add('buttonsEditMain');
-        this.#button2.addEventListener('click', (e) => {this.bin(e)})
+        this.#button2.addEventListener('click', (e) => {
+            this.bin(e)
+        })
+
 
         this.#div.append(this.#input);
         this.#div.append(this.#p);
+
         this.#buttonDiv.append(this.#button1);
         this.#buttonDiv.append(this.#button2);
+
         this.#mainDiv.append(this.#div);
         this.#mainDiv.append(this.#buttonDiv);
+        this.#mainDiv.append(this.#nav);
 
         return this.#mainDiv;
     }
-    additionalSettings(e){
-        this.#userY = e.pageY;
-        this.#element = document.querySelector('.addSett')
-        if (this.#element.style.display === 'none'){
-            this.#element.classList.add('addSett');
-            this.#element.style.top = `${this.#userY}px`;
-            this.#element.style.display = 'flex';
-            this.#element.style.flexDirection = 'column';
 
-            document.querySelector('#complete').addEventListener('click',
-                (e) => {this.taskCompleted(e)})
-        }else{
-            this.#element.style.display = 'none';
-        }
-    }
-
-    taskCompleted(e){
-        if(this.#input.checked){
+    taskCompleted(e) {
+        if (this.#input.checked) {
             this.#mainDiv.classList.add('completed');
             this.#button1.classList.add('completed');
             this.#button2.classList.add('completed');
             document.querySelector('.ready').append(this.#mainDiv)
-        }else {
+        } else {
             this.#mainDiv.classList.remove('completed');
             this.#button1.classList.remove('completed');
             this.#button2.classList.remove('completed');
             this.#input.checked = false;
             document.querySelector('.taskList').append(this.#mainDiv)
         }
+        this.#element.style.display = 'none';
     }
 
-    bin(e){
+    bin(e) {
         document.querySelector('.bin').append(this.#mainDiv)
         this.#input.checked = true
         if (this.#mainDiv.classList.contains('completed')) {
             this.#button2.style.display = 'none';
             this.#input.style.display = 'none'
-        } else{
+        } else {
             this.#mainDiv.classList.add('delete');
             this.#button1.classList.add('delete');
             this.#button2.classList.add('delete');
             this.#button2.style.display = 'none';
             this.#input.style.display = 'none'
         }
+        this.#element.style.display = 'none';
+    }
+
+    edit(el) {
+        let enter = document.createElement('input');
+        let help = document.createElement('p');
+
+        this.#div.append(enter);
+        this.#div.append(help);
+        help.textContent = 'Press Enter to submit a  changes'
+        help.style.marginLeft = '15px'
+
+        enter.value = this.#task;
+        enter.style.border = 'solid 1px green'
+        enter.style.borderRadius= '12px'
+        el.style.display = 'none';
+        enter.addEventListener('keypress', (e) => {
+            if (e.code === 'Enter') {
+                enter.style.display = 'none';
+                help.style.display = 'none'
+                el.textContent = enter.value;
+                el.style.display = '';
+            }
+        });
     }
 }
-
 let night = document.querySelector('.moon');
 night.addEventListener('click', function (){
     document.querySelector('body').classList.toggle('nightMode');
